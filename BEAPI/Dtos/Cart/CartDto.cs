c1de8362f6cc0b2a044ea547e2c20640601d3733
ttson1001 <74534768+ttson1001@users.noreply.github.com>
2025-08-12 02:37:31 +0700

    oka

--- BEAPI/Dtos/Cart/CartDto.cs
+++ BEAPI/Dtos/Cart/CartDto.cs
@@ -1,4 +1,6 @@
-﻿namespace BEAPI.Dtos.Cart
+﻿using BEAPI.Entities.Enum;
+
+namespace BEAPI.Dtos.Cart
 {
     public class CartDto
     {
@@ -7,7 +9,7 @@
         public string CustomerName { get; set; } = null!;
         public Guid? ElderId { get; set; }
         public string? ElderName { get; set; }
-        public string Status { get; set; } = null!;
+        public CartStatus Status { get; set; }
         public List<CartItemDto> Items { get; set; } = new();
     }
 
