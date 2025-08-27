f89fefe70206f6315c68f19e211cd543994c1649
ttson1001 <74534768+ttson1001@users.noreply.github.com>
2025-08-28 00:06:26 +0700

    28/8/2025-12-06

---
+++ BEAPI/Dtos/Category/UpdateCategoryValueDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BEAPI.Dtos.Category
+{
+    public class UpdateCategoryValueDto
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Label { get; set; } = string.Empty;
+    }
+}
